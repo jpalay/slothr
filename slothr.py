e6dce35b923e1d51c2efc556e2d421ce28beba0e
Josh Palay <joshpalay@gmail.com>
2014-01-10 14:34:23 -0600

    Added get_by_id route

--- slothr.py
+++ slothr.py
@@ -1,6 +1,6 @@
 import mimetypes
 
-from flask import Flask, send_file
+from flask import Flask, abort, send_file
 
 from models import FlickrImage, CroppedFlickrImage, Session
 s = Session()
@@ -10,8 +10,8 @@ app.config.from_object('settings')
 
 @app.route('/<int:width>/<int:height>/')
 def get_image(width, height):
-    ratio = float(width) / height
     s = Session()
+    ratio = float(width) / height
     imgs = []
     for img in s.query(FlickrImage).all():
         imgs.append({
@@ -20,10 +20,22 @@ def get_image(width, height):
         })
     imgs = sorted(imgs, key=lambda d: d['ratio_diff'])
     img = imgs[0]['img']
-    cropped_img = img.get_cropped(width, height, s)
-    sf = send_file(cropped_img.path, mimetypes.guess_type(cropped_img.path)[0])
+    cropped = img.get_cropped(width, height, s)
+    sf = send_file(cropped.path, mimetypes.guess_type(cropped.path)[0])
     s.close()
     return sf
+
+@app.route('/<int:img_id>/<int:width>/<int:height>/')
+def get_by_id(img_id, width, height):
+    s = Session()
+    img = s.query(FlickrImage).filter(FlickrImage.id==img_id).first()
+    if not img:
+        abort(404)
+    cropped = img.get_cropped(width, height, s)
+    sf = send_file(cropped.path, mimetypes.guess_type(cropped.path)[0])
+    s.close()
+    return sf
+
 
 if __name__ == '__main__':
     app.debug = True
